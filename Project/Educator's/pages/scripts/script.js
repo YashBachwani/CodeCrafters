7e002ddc4918765c1b6a975c7e455b3b20e45036
YashBachwani <yash.bachwani30@gmail.com>
2025-02-09 13:49:28 +0530

    Intro Video and website is loaded

---
+++ Project/Educator's/pages/scripts/script.js
@@ -0,0 +1,132 @@
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn1')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content1");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn2')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content2");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn3')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content3");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  function myFunction4() {
+    document.getElementById("myDropdown4").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn4')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content4");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+// NavBar
+let menulist = document.getElementById("menulist")
+  menulist.style.maxHeight = 0;
+  function toggleMenu(){
+    if(menulist.style.maxHeight == "0px"){
+      menulist.style.maxHeight = "1000px";
+    }
+    else{
+      menulist.style.maxHeight = "0px";
+    }
+}
+
+
+// For preloader
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
+
+
+// Custome Cursor 
+const cursorDot = document.querySelector("[data-cursor-dot");
+const cursorOutline = document.querySelector("[data-cursor-outline");
+
+window.addEventListener("mousemove",function(e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+    
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left : `${posX}px`,
+        top: `${posY}px`
+    },{duration:500, fill:"forwards"})
+
+});
